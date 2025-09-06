857908560196c15d3896ec955fb16f6b061f4633
Om Gunturkar <omgunturkar@gmail.com>
2025-09-06 15:15:21 +0530

    Updated

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
+
+const reviews = require("./routes/review.js");
 
 const listings = require("./routes/listing.js")
 
@@ -38,56 +37,8 @@ app.get("/", (req, res) => {
   res.send("Hii, I am root");
 });
 
-
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
-
-app.use("/listings",listings)
-
-// Reviews
-
-//POST review route
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-  })
-);
-
-//DELETE review route
-
-app.delete(
-  "/listings/:id/reviews/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, {$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-
-
-    res.redirect(`/listings/${id}`);
-  })
-);
-
-// app.all('*',(req,res,next)=>{
-//     next(new ExpressError(404,"page Not Found"))
-// })
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews);
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something Went wrong" } = err;
